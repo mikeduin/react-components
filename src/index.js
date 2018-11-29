7aa52314f2337ba5526de3972a081b2efafd2496
Mike Duin <michael.s.duin@gmail.com>
2018-11-28 22:58:21 -0800

    add approvalcard + nested props

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        date="Today at 4:50PM"
-        post="Hahaha!"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Mike"
-        date="Today at 2:00AM"
-        post="This is a great app!"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Hannah"
-        date="Yesterday at 5:00PM"
-        post="This thing sucks."
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date="Today at 4:50PM"
+          post="Hahaha!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Mike"
+          date="Today at 2:00AM"
+          post="This is a great app!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Hannah"
+          date="Yesterday at 5:00PM"
+          post="This thing sucks."
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
